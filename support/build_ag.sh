364b2b8d0a854d7ef23905b78c1ba005758809bb
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-12-23 06:29:13 -0600

    latest ag,watchman

--- support/build_ag.sh
+++ support/build_ag.sh
@@ -4,6 +4,6 @@ mkdir -p /docker-build/support/Downloads
 cd /docker-build/support/Downloads
 git clone https://github.com/ggreer/the_silver_searcher.git
 cd /docker-build/support/Downloads/the_silver_searcher
-git checkout 0.24.1
+git checkout 0.27.0
 ./build.sh
 make install
--- support/build_watchman.sh
+++ support/build_watchman.sh
@@ -4,7 +4,7 @@ mkdir -p /docker-build/support/Downloads
 cd /docker-build/support/Downloads
 git clone https://github.com/facebook/watchman.git
 cd /docker-build/support/Downloads/watchman
-git checkout v2.9.8
+git checkout v3.0.0
 ./autogen.sh
 ./configure
 make
