e354160fbc0354ded61fd6824eba9f951ebd4e08
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2014-12-23 06:29:03 -0600

    .

--- support/user_common.sh
+++ support/user_common.sh
@@ -4,3 +4,5 @@ cat "/docker-build/support/bashrc_append_$(whoami).txt" >> $HOME/.bashrc
 cat /docker-build/support/bashrc_append_common.txt >> $HOME/.bashrc
 cp /docker-build/support/tmux.conf $HOME/.tmux.conf
 chmod 644 $HOME/.tmux.conf
+
+git clone https://github.com/revans/bash-it.git $HOME/.bash_it
