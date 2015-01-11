d1d1aff611a87f4e40bf25e20758bec0b38ff4ba
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-01-11 03:37:58 -0600

    default for .entry_env is to exist but be empty

--- support/user_common.sh
+++ support/user_common.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+touch $HOME/.entry_env
+
 cat "/docker-build/support/bashrc_append_$(whoami).txt" >> $HOME/.bashrc
 cat /docker-build/support/bashrc_append_common.txt >> $HOME/.bashrc
 cp /docker-build/support/tmux.conf $HOME/.tmux.conf
