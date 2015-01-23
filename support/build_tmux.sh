83c4b7024d363199ae4d7c44e9b9cf8200e906bd
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-01-23 02:57:41 -0600

    use ncurses dev pkg installed w/ apt-get

--- support/build_tmux.sh
+++ support/build_tmux.sh
@@ -14,12 +14,12 @@ tmux_version="1.9"
 tmux_patch_version="a" # leave empty for stable releases
 
 libevent_version="2.0.21"
-ncurses_version="5.9"
+# ncurses_version="5.9"
 
 tmux_name="tmux-$tmux_version"
 tmux_relative_url="$tmux_name/$tmux_name$tmux_patch_version"
 libevent_name="libevent-$libevent_version-stable"
-ncurses_name="ncurses-$ncurses_version"
+# ncurses_name="ncurses-$ncurses_version"
 
 # set the installation directory
 
@@ -32,7 +32,7 @@ cd /tmp
 
 wget -O $tmux_name.tar.gz http://sourceforge.net/projects/tmux/files/tmux/$tmux_relative_url.tar.gz/download
 wget -O $libevent_name.tar.gz https://github.com/downloads/libevent/libevent/$libevent_name.tar.gz
-wget -O $ncurses_name.tar.gz ftp://ftp.gnu.org/gnu/ncurses/$ncurses_name.tar.gz
+# wget -O $ncurses_name.tar.gz ftp://ftp.gnu.org/gnu/ncurses/$ncurses_name.tar.gz
 
 # extract files, configure, and compile
 
@@ -45,18 +45,25 @@ make install
 cd -
 
 # ncurses installation
-tar xvzf $ncurses_name.tar.gz
-cd $ncurses_name
-./configure --prefix=$target_dir
-make
-make install
-cd -
+# tar xvzf $ncurses_name.tar.gz
+# cd $ncurses_name
+# ./configure --prefix=$target_dir
+# make
+# make install
+# cd -
 
 # tmux installation
 tar xvzf ${tmux_name}*.tar.gz
 cd ${tmux_name}*/
-./configure CFLAGS="-I$target_dir/include -I$target_dir/include/ncurses" LDFLAGS="-L$target_dir/lib -L$target_dir/include/ncurses -L$target_dir/include"
-CPPFLAGS="-I$target_dir/include -I$target_dir/include/ncurses" LDFLAGS="-static -L$target_dir/include -L$target_dir/include/ncurses -L$target_dir/lib" make
+
+# ./configure CFLAGS="-I$target_dir/include -I$target_dir/include/ncurses" LDFLAGS="-L$target_dir/lib -L$target_dir/include/ncurses -L$target_dir/include"
+
+./configure CFLAGS="-I$target_dir/include -I/usr/include" LDFLAGS="-L$target_dir/lib -L/usr/include -L$target_dir/include"
+
+# CPPFLAGS="-I$target_dir/include -I$target_dir/include/ncurses" LDFLAGS="-static -L$target_dir/include -L$target_dir/include/ncurses -L$target_dir/lib" make
+
+CPPFLAGS="-I$target_dir/include -I/usr/include" LDFLAGS="-static -L$target_dir/include -L/usr/include -L$target_dir/lib" make
+
 cp tmux $target_dir/bin
 cd -
 
