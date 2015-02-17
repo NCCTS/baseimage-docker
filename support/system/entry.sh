527b49b3198e1afdf138db9eaaf29fa9323cb410
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-02-17 05:39:32 -0600

    shape up sudo commands

--- support/system/entry.sh
+++ support/system/entry.sh
@@ -607,42 +607,44 @@ fi
 if [ "$ENTRY_TMUX" = true ]; then
     if [ -n "$entry_cmd" ]; then
         tmux_cmd=(tmux new-session -A "$entry_session_flag" "$entry_cmd")
-        sudo_cmd=(sudo -i -u \
-                       "$ENTRY_LOGIN" \
-                       "$entry_filter_final" \
-                       "ENTRY_BASH_ENV=" \
-                       "$entry_bash_env_preserve" \
-                       "BASH_ENV=$entry_env_home/.bash_env_wrap" \
-                       bash -c \
-                       "$(printf "%q " "${tmux_cmd[@]}")")
-        eval "$(printf "%q " "${sudo_cmd[@]}")"
+        sudo_cmd_1=(sudo -i -u
+                    "$ENTRY_LOGIN"
+                    "$entry_filter_final"
+                    "ENTRY_BASH_ENV="
+                    "$entry_bash_env_preserve"
+                    "BASH_ENV=$entry_env_home/.bash_env_wrap")
+        sudo_cmd_2=(bash -c
+                    "$(printf "%q " "${tmux_cmd[@]}")")
+        eval "${sudo_cmd_1[@]} $(printf "%q " "${sudo_cmd_2[@]}")"
     else
-        sudo -i -u \
-             "$ENTRY_LOGIN" \
-             "$entry_filter_final" \
-             "ENTRY_BASH_ENV=" \
-             "$entry_bash_env_preserve" \
-             "BASH_ENV=$entry_env_home/.bash_env_wrap" \
-             tmux new-session -A "$entry_session_flag" "$entry_empty_cmd"
+        sudo_cmd=(sudo -i -u
+                  "$ENTRY_LOGIN"
+                  "$entry_filter_final"
+                  "ENTRY_BASH_ENV="
+                  "$entry_bash_env_preserve"
+                  "BASH_ENV=$entry_env_home/.bash_env_wrap"
+                  tmux new-session -A "$entry_session_flag" "$entry_empty_cmd")
+        eval "${sudo_cmd[@]}"
     fi
 else
     if [ -n "$entry_cmd" ]; then
-        sudo_cmd=(sudo -i -u \
-                       "$ENTRY_LOGIN" \
-                       "$entry_filter_final" \
-                       "ENTRY_BASH_ENV=" \
-                       "$entry_bash_env_preserve" \
-                       "BASH_ENV=$entry_env_home/.bash_env_wrap" \
-                       bash -c \
-                       "$entry_cmd")
-        eval "$(printf "%q " "${sudo_cmd[@]}")"
+        sudo_cmd=(sudo -i -u
+                  "$ENTRY_LOGIN"
+                  "$entry_filter_final"
+                  "ENTRY_BASH_ENV="
+                  "$entry_bash_env_preserve"
+                  "BASH_ENV=$entry_env_home/.bash_env_wrap"
+                  bash -c
+                  "$(printf "%q " "$entry_cmd")")
+        eval "${sudo_cmd[@]}"
     else
-        sudo -i -u \
-             "$ENTRY_LOGIN" \
-             "$entry_filter_final" \
-             "ENTRY_BASH_ENV=" \
-             "$entry_bash_env_preserve" \
-             "BASH_ENV=$entry_env_home/.bash_env_wrap" \
-             "$entry_empty_cmd"
+        sudo_cmd=(sudo -i -u
+                  "$ENTRY_LOGIN"
+                  "$entry_filter_final"
+                  "ENTRY_BASH_ENV="
+                  "$entry_bash_env_preserve"
+                  "BASH_ENV=$entry_env_home/.bash_env_wrap"
+                  "$entry_empty_cmd")
+        eval "${sudo_cmd[@]}"
     fi
 fi
