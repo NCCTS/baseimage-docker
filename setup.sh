7bf0517c9e4f8fca994a3c0a8cdcd2c7c38ce04a
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-02-19 01:20:47 -0600

    build before user config

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ export DEBIAN_FRONTEND=noninteractive
 # packages,system config
 /docker-build/support/config.sh
 
-# user config
-/docker-build/support/user.sh
-
 # build,install from sources
 /docker-build/support/build.sh
+
+# user config
+/docker-build/support/user.sh
 
 # cleanup
 /docker-build/support/cleanup.sh
